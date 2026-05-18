9fd9704aebb7aef7a8c4013e99832ddfeb41d797
Shirasawa-Sota-TP <282408073+Shirasawa-Sota-TP@users.noreply.github.com>
2026-05-18 16:20:05 +0900

    rental

---
+++ src/rental.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+enum Button {
+    ホーム,
+    戻る,
+    貸出,
+};
+enum RentalStatus {
+    貸出可能,
+    貸出不可,
+    貸出中,
+    延滞中
+};
+typedef struct {
+    int book_id;
+    char title[100];
+    char author[100];
+    enum RentalStatus status;
+} Bookinfo;
+void rental(void){
+    Bookinfo b_info;
+    scanf("%d %s %s %d",&b_info.book_id, b_info.title, b_info.author, &b_info.status);    
+    printf("書籍ID: %d\n", b_info.book_id);
+    printf("タイトル: %s\n", b_info.title);
+    printf("著者: %s\n", b_info.author);
+
+    printf("貸出状況: %s\n", b_info.status == 貸出可能 ? "貸出可能" : b_info.status == 貸出不可 ? "貸出不可" : b_info.status == 貸出中 ? "貸出中" : "延滞中");
+    
+
+}
