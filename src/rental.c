7eb4c4e045d6a1e45cb5c32eeb9efa77c6710e59
Ono Kotaro <282407994+Ono-Kotaro-TP@users.noreply.github.com>
2026-05-18 17:33:23 +0900

    入力内容の表示を追加

--- src/rental.c
+++ src/rental.c
@@ -19,12 +19,11 @@ typedef struct {
 } Bookinfo;
 void rental(void){
     Bookinfo b_info;
+    printf("整数 文字列 文字列 整数(0-3)のように入力してください: ");
     scanf("%d %s %s %d",&b_info.book_id, b_info.title, b_info.author, &b_info.status);    
     printf("書籍ID: %d\n", b_info.book_id);
     printf("タイトル: %s\n", b_info.title);
     printf("著者: %s\n", b_info.author);
 
     printf("貸出状況: %s\n", b_info.status == 貸出可能 ? "貸出可能" : b_info.status == 貸出不可 ? "貸出不可" : b_info.status == 貸出中 ? "貸出中" : "延滞中");
-    
-
 }
