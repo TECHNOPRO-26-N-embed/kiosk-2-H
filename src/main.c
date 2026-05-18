335a22afcbdf7a4de742e650cef6bc61c6d28b47
Ono Kotaro <282407994+Ono-Kotaro-TP@users.noreply.github.com>
2026-05-18 17:09:11 +0900

    main関数をwhileで常時受け付けるよう変更

--- src/main.c
+++ src/main.c
@@ -6,23 +6,25 @@
 #include "history.c"
 
 int main(void) {
-    printf("メニュー\n");
-    printf("1: 貸出\n");
-    printf("2: 返却\n");
-    printf("3: 検索\n");
-    printf("4: 貸出履歴\n");
-    
-    printf("1-4の数字を入力してください: ");
-    int n;
-    scanf("%d",&n);
+    while(1) {
+        printf("メニュー\n");
+        printf("1: 貸出\n");
+        printf("2: 返却\n");
+        printf("3: 検索\n");
+        printf("4: 貸出履歴\n");
+        
+        printf("1-4の数字を入力してください: ");
+        int n;
+        scanf("%d",&n);
 
-    // 条件に応じて各処理の関数を呼び出す。
-    if (n == 1) {rental();}
-    else if (n == 2) {return_book();}
-    else if (n == 3) {search();}
-    else if (n == 4) {history();}
-    else {
-        printf("1-4の数字を入力してください; \n");
+        // 条件に応じて各処理の関数を呼び出す。
+        if (n == 1) {rental();}
+        else if (n == 2) {return_book();}
+        else if (n == 3) {search();}
+        else if (n == 4) {history();}
+        else {
+            printf("1-4の数字を入力してください; \n");
+        }
     }
 
     return 0;
